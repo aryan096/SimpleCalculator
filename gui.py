23e4156adc21ad3a84702bc137002792426937ba
aryan096 <aryansrivastava096@gmail.com>
2018-07-20 11:27:06 +0530

    Initial conventional calc. Bugs exist.

--- gui.py
+++ gui.py
@@ -25,17 +25,20 @@ class CalcLayout(tk.Frame):
     Entry fields
     Operation Buttons
     '''
-    def create_widgets(self):
+    def create_widgets(self):s
 
+        self.currentAns = 0
         self.outputNum = tk.StringVar()
         self.firstNum = tk.StringVar()
-        self.secondNum = tk.StringVar()
+        self.operation_message = tk.StringVar()
+        self.current_op = 0
+
+        self.operation = tk.Message(self, textvariable=self.operation_message, width=80, justify="center")
+        self.operation.grid(row=0, columnspan=2)
 
         #Enter Field
-        self.entry1 = tk.Entry(self, textvariable=self.firstNum, width=10, justify="center", bd=4, selectborderwidth=3)
-        self.entry1.grid(row=0, column=0)
-        self.entry2 = tk.Entry(self, textvariable=self.secondNum, width=10, justify="center", bd=4, selectborderwidth=3)
-        self.entry2.grid(row=0, column=1)
+        self.entry1 = tk.Entry(self, textvariable=self.firstNum, width=20, justify="center", bd=4, selectborderwidth=3)
+        self.entry1.grid(row=1, columnspan=2)
 
         #Operation Buttons
         self.add_button = tk.Button(self, width=10, text="+", bg ="grey", command= lambda: self.set_output(type='+'))
@@ -46,23 +49,76 @@ class CalcLayout(tk.Frame):
         self.mult_button.grid(row=4, column=0)
         self.divide_button = tk.Button(self, width=10, text="/", bg="grey", command= lambda: self.set_output(type='/'))
         self.divide_button.grid(row=4, column=1)
+        self.equal_button = tk.Button(self, width=50, text="=", bg="grey", command= self.set_result)
+        self.equal_button.grid(row=5, columnspan=2)
 
-        self.output = tk.Message(self, width=100, textvariable=self.outputNum)
-        self.output.grid(row=2, columnspan=2)
-        self.outputNum.set("INITIAL")
+        #self.current = tk.Message(self, width=50, textvariable=self.outputNum)
+        #self.current.grid(row=0, column=2, sticky="w")
+        #self.outputNum.set("INITIAL")
 
 
     def set_output(self, type):
 
         if(type=='+'):
-            self.outputNum.set(int(self.entry1.get())+int(self.entry2.get()))
-            self.master.update_idletasks()
+            try:
+                self.currentAns = self.currentAns + int(self.entry1.get())
+                self.operation_message.set(str(self.currentAns) + " +")
+                self.firstNum.set(self.currentAns)
+                self.current_op = 0
+                self.firstNum.set("")
+                self.master.update_idletasks()
+            finally:
+                self.operation_message.set(str(self.currentAns) + " +")
+                self.current_op = 0
+
         elif(type=='-'):
-            self.outputNum.set(int(self.entry1.get())-int(self.entry2.get()))
-            self.master.update_idletasks()
+            try:
+                self.currentAns = self.currentAns - int(self.entry1.get())
+                self.operation_message.set(str(self.currentAns) + " -")
+                self.firstNum.set(self.currentAns)
+                self.current_op = 1
+                self.firstNum.set("")
+                self.master.update_idletasks()
+            finally:
+                self.operation_message.set(str(self.currentAns) + " -")
+                self.current_op = 1
+
         elif(type=='*'):
-            self.outputNum.set(int(self.entry1.get())*int(self.entry2.get()))
-            self.master.update_idletasks()
+            try:
+                self.currentAns = self.currentAns * int(self.entry1.get())
+                self.operation_message.set(str(self.currentAns) + " *")
+                self.firstNum.set(self.currentAns)
+                self.current_op = 2
+                self.firstNum.set("")
+                self.master.update_idletasks()
+            finally:
+                self.operation_message.set(str(self.currentAns) + " *")
+                self.current_op = 2
+
         elif(type=='/'):
-            self.outputNum.set(int(self.entry1.get())/int(self.entry2.get()))
-            self.master.update_idletasks()
+            try:
+                self.currentAns = self.currentAns / int(self.entry1.get())
+                self.operation_message.set(str(self.currentAns) + " /")
+                self.firstNum.set(self.currentAns)
+                self.current_op = 3
+                self.firstNum.set("")
+                self.master.update_idletasks()
+            finally:
+                self.operation_message.set(str(self.currentAns) + " /")
+                self.current_op = 3
+
+    def set_result(self):
+        if self.current_op == 0:
+            self.operation_message.set(self.currentAns + int(self.entry1.get()))
+            self.currentAns = self.currentAns + int(self.entry1.get())
+        elif self.current_op == 1:
+            self.operation_message.set(self.currentAns - int(self.entry1.get()))
+            self.currentAns = self.currentAns - int(self.entry1.get())
+        elif self.current_op == 2:
+            self.operation_message.set(self.currentAns * int(self.entry1.get()))
+            self.currentAns = self.currentAns * int(self.entry1.get())
+        elif self.current_op == 3:
+            self.operation_message.set(self.currentAns / int(self.entry1.get()))
+            self.currentAns = self.currentAns / int(self.entry1.get())
+        self.firstNum.set("")
+        self.master.update_idletasks()
